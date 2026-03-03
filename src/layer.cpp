e30c4e47295144bfa345295f8d2d75ff4b5204c9
Dmitrii-01 <144121834+Dmitrii-01@users.noreply.github.com>
2026-03-03 23:16:59 +0300

    Layer implemented

--- src/layer.cpp
+++ src/layer.cpp
@@ -1,1 +1,17 @@
+#include "layer.hpp"
+#include <stdexcept>
 
+Layer::Layer(size_t n) {
+    this->n = n;
+    neurons = new float[n]{};
+}
+
+Layer::~Layer() {
+    delete[] neurons;
+}
+
+float& Layer::operator[](size_t idx) {
+    if (idx > n)
+        throw std::out_of_range("layer index out of range");
+    return neurons[idx];
+}
