5b8aeb30d386c2ca9dafc872b58cebcea3c4cfeb
Dmitrii-01 <144121834+Dmitrii-01@users.noreply.github.com>
2026-03-10 18:32:16 +0300

    updated all headers, updated CMake

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstddef>
+#include <initializer_list>
+#include <functional>
 #include "vector.hpp"
 
 using Row = Vector;
@@ -8,11 +10,29 @@ using Row = Vector;
 class Matrix {
 private:
     size_t n_rows, n_cols;
-    Row* matrix = nullptr;
+    Row* matrix;
+    Matrix(size_t, size_t, Row*);
+    void swap(Matrix&) noexcept;
+    Matrix apply_op(const Matrix&, float(*)(const float&, const float&)) const;
 public:
-    Matrix() = default;
-    Matrix(size_t, size_t); // num. of rows and num. of columns
+    Matrix();
+    Matrix(size_t, size_t);
+    Matrix(std::initializer_list<Row>);
+    Matrix(const Matrix&);
+    Matrix(Matrix&&) noexcept;
     ~Matrix();
+    Matrix& operator=(Matrix);
+    const Row& operator[](size_t) const;
     Row& operator[](size_t);
-    Vector operator*(const Vector&);
+    Matrix operator-(const Matrix&) const;
+    Matrix& operator-=(const Matrix&);
+    Vector operator*(const Vector&) const;
+    size_t rows() const;
+    size_t cols() const;
+    Row* data();
+    const Row* data() const;
 };
+
+Matrix diag(const Vector&);
+Matrix outer_product(const Vector&, const Vector&);
+Vector operator*(const Vector&, const Matrix&);
