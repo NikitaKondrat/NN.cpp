87bcc22793c9de5b49ea2e3ede14e697fe4a9302
Dmitrii-01 <144121834+Dmitrii-01@users.noreply.github.com>
2026-03-10 18:32:16 +0300

    updated all headers, updated CMake

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,16 +1,37 @@
 #pragma once
 
 #include <cstddef>
+#include <initializer_list>
+#include <functional>
+
+using FtoF = float(*)(float);
 
 class Vector {
 private:
     size_t n;
-    float* values = nullptr;
+    float* values;
+    Vector(size_t, float*);
+    void swap(Vector&) noexcept;
+    Vector apply_op(const Vector&, float(*)(const float&, const float&)) const;
 public:
-    Vector() = default;
+    Vector();
     Vector(size_t);
+    Vector(std::initializer_list<float>);
+    Vector(const Vector&);
+    Vector(Vector&&) noexcept;
     ~Vector();
+    Vector& operator=(Vector);
+    const float& operator[](size_t) const;
     float& operator[](size_t);
-    Vector(const Vector& other);
-    Vector& operator=(const Vector& other);
+    Vector operator+(const Vector&) const;
+    Vector& operator+=(const Vector&);
+    Vector operator-(const Vector&) const;
+    Vector& operator-=(const Vector&);
+    Vector& apply(FtoF);
+    Vector map(FtoF) const;
+    size_t size() const;
+    float* data();
+    const float* data() const;
 };
+
+Vector hadamar(const Vector&, const Vector&);
